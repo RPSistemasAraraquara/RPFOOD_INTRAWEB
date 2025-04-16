7200822d97db7702efe86a06166994a408928aae
DESKTOP-O3030MS\RAFAEL <Don Rafael>
2025-04-16 13:11:14 -0300

    Add TRPFoodEntityConfiguracaoPagamentoMercadoPago

--- Fontes/Source/Model/Entity/RPFood.Entity.Classes.pas
+++ Fontes/Source/Model/Entity/RPFood.Entity.Classes.pas
@@ -22,37 +22,39 @@ uses
   RPFood.Entity.TransferenciaImagens,
   RPFood.Entity.ConfiguracaoRPFood,
   RPFood.Entity.Happy_Hour,
-  RPFood.Entity.RestricaoVenda;
+  RPFood.Entity.RestricaoVenda,
+  RPFood.Entity.Configuracao.PagamentoMercadoPago;
 
 type
-  TRPFoodEntityADMINUsuario             = RPFood.Entity.ADMIN.Usuario.TRPFoodEntityADMINUsuario;
-  TRPFoodEntityBairro                   = RPFood.Entity.Bairro.TRPFoodEntityBairro;
-  TRPFoodEntityCategoria                = RPFood.Entity.Categoria.TRPFoodEntityCategoria;
-  TRPFoodEntityCliente                  = RPFood.Entity.Cliente.TRPFoodEntityCliente;
-  TRPFoodEntityClienteEndereco          = RPFood.Entity.Endereco.TRPFoodEntityClienteEndereco;
-  TRPFoodEntityEmpresa                  = RPFood.Entity.Empresa.TRPFoodEntityEmpresa;
-  TRPFoodEntityEndereco                 = RPFood.Entity.Endereco.TRPFoodEntityEndereco;
-  TRPFoodEntityFormaPagamento           = RPFood.Entity.FormaPagamento.TRPFoodEntityFormaPagamento;
-  TRPFoodEntityOpcional                 = RPFood.Entity.Opcional.TRPFoodEntityOpcional;
-  TRPFoodEntityPedido                   = RPFood.Entity.Pedido.TRPFoodEntityPedido;
-  TRPFoodEntityPedidoItem               = RPFood.Entity.Pedido.TRPFoodEntityPedidoItem;
-  TRPFoodEntityPedidoItemFracao         = RPFood.Entity.Pedido.TRPFoodEntityPedidoItemFracao;
-  TRPFoodEntityPedidoItemOpcional       = RPFood.Entity.Pedido.TRPFoodEntityPedidoItemOpcional;
-  TRPFoodEntityProduto                  = RPFood.Entity.Produto.TRPFoodEntityProduto;
-  TRPFoodEntityProdutoOpcional          = RPFood.Entity.ProdutoOpcional.TRPFoodEntityProdutoOpcional;
-  TRPFoodEntityVenda                    = RPFood.Entity.Venda.TRPFoodEntityVenda;
-  TRPFoodEntityVendaEndereco            = RPFood.Entity.VendaEndereco.TRPFoodEntityVendaEndereco;
-  TRPFoodEntityVendaStatusLog           = RPFood.Entity.VendaStatusLog.TRPFoodEntityVendaStatusLog;
-  TRPFoodEntityVendaItem                = RPFood.Entity.Venda.TRPFoodEntityVendaItem;
-  TRPFoodEntityVendaItemOpcional        = RPFood.Entity.Venda.TRPFoodEntityVendaItemOpcional;
-  TRPFoodEntityRelatorioVendas          = RPFood.Entity.RelatorioVendas.TRPFoodEntityRelatorioVendas;
-  TRPFoodEntityConfiguracaoAtendimento  = RPFood.Entity.ConfiguracaoAtendimento.TRPFoodEntityConfiguracaAtendimento;
-  TRPFoodEntityHorarioFuncionamento     = RPFood.Entity.ConfiguracaoAtendimento.TRPFoodEntityHorarioFuncionamento;
-  TRPFoodEntityTransferenciaImagens     = RPFood.Entity.TransferenciaImagens.TRPFoodEntityTransferenciaImagens;
-  TRPFoodEntityConfiguracaoRPFood       = RPFood.Entity.ConfiguracaoRPFood.TRPFoodEntityConfiguracaoRPFood;
-  TRPFoodEntityHappy_Hour               = RPFood.Entity.Happy_Hour.TRPFoodEntityHappy_Hour;
-  TRPFoodEntityRestricaoVenda           = RPFood.Entity.RestricaoVenda.TRPFoodEntityRestricaoVenda;
-  TRPFoodEntityPromocao                 = RPFood.Entity.Produto.TRPFoodEntityPromocao;
+  TRPFoodEntityADMINUsuario                       = RPFood.Entity.ADMIN.Usuario.TRPFoodEntityADMINUsuario;
+  TRPFoodEntityBairro                             = RPFood.Entity.Bairro.TRPFoodEntityBairro;
+  TRPFoodEntityCategoria                          = RPFood.Entity.Categoria.TRPFoodEntityCategoria;
+  TRPFoodEntityCliente                            = RPFood.Entity.Cliente.TRPFoodEntityCliente;
+  TRPFoodEntityClienteEndereco                    = RPFood.Entity.Endereco.TRPFoodEntityClienteEndereco;
+  TRPFoodEntityEmpresa                            = RPFood.Entity.Empresa.TRPFoodEntityEmpresa;
+  TRPFoodEntityEndereco                           = RPFood.Entity.Endereco.TRPFoodEntityEndereco;
+  TRPFoodEntityFormaPagamento                     = RPFood.Entity.FormaPagamento.TRPFoodEntityFormaPagamento;
+  TRPFoodEntityOpcional                           = RPFood.Entity.Opcional.TRPFoodEntityOpcional;
+  TRPFoodEntityPedido                             = RPFood.Entity.Pedido.TRPFoodEntityPedido;
+  TRPFoodEntityPedidoItem                         = RPFood.Entity.Pedido.TRPFoodEntityPedidoItem;
+  TRPFoodEntityPedidoItemFracao                   = RPFood.Entity.Pedido.TRPFoodEntityPedidoItemFracao;
+  TRPFoodEntityPedidoItemOpcional                 = RPFood.Entity.Pedido.TRPFoodEntityPedidoItemOpcional;
+  TRPFoodEntityProduto                            = RPFood.Entity.Produto.TRPFoodEntityProduto;
+  TRPFoodEntityProdutoOpcional                    = RPFood.Entity.ProdutoOpcional.TRPFoodEntityProdutoOpcional;
+  TRPFoodEntityVenda                              = RPFood.Entity.Venda.TRPFoodEntityVenda;
+  TRPFoodEntityVendaEndereco                      = RPFood.Entity.VendaEndereco.TRPFoodEntityVendaEndereco;
+  TRPFoodEntityVendaStatusLog                     = RPFood.Entity.VendaStatusLog.TRPFoodEntityVendaStatusLog;
+  TRPFoodEntityVendaItem                          = RPFood.Entity.Venda.TRPFoodEntityVendaItem;
+  TRPFoodEntityVendaItemOpcional                  = RPFood.Entity.Venda.TRPFoodEntityVendaItemOpcional;
+  TRPFoodEntityRelatorioVendas                    = RPFood.Entity.RelatorioVendas.TRPFoodEntityRelatorioVendas;
+  TRPFoodEntityConfiguracaoAtendimento            = RPFood.Entity.ConfiguracaoAtendimento.TRPFoodEntityConfiguracaAtendimento;
+  TRPFoodEntityHorarioFuncionamento               = RPFood.Entity.ConfiguracaoAtendimento.TRPFoodEntityHorarioFuncionamento;
+  TRPFoodEntityTransferenciaImagens               = RPFood.Entity.TransferenciaImagens.TRPFoodEntityTransferenciaImagens;
+  TRPFoodEntityConfiguracaoRPFood                 = RPFood.Entity.ConfiguracaoRPFood.TRPFoodEntityConfiguracaoRPFood;
+  TRPFoodEntityHappy_Hour                         = RPFood.Entity.Happy_Hour.TRPFoodEntityHappy_Hour;
+  TRPFoodEntityRestricaoVenda                     = RPFood.Entity.RestricaoVenda.TRPFoodEntityRestricaoVenda;
+  TRPFoodEntityPromocao                           = RPFood.Entity.Produto.TRPFoodEntityPromocao;
+  TRPFoodEntityConfiguracaoPagamentoMercadoPago   = RPFood.Entity.Configuracao.PagamentoMercadoPago.TRPFoodEntityConfiguracaoPagamentoMercadoPago;
 
 implementation
 
